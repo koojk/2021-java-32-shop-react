f01420ab63b26beef95b895a166eb00bc661f73a
koojk <kljh0881@gmail.com>
2021-11-26 12:26:01 +0900

    NaviCp

--- src/components/inc/NaviCp.js
+++ src/components/inc/NaviCp.js
@@ -1,10 +1,19 @@
 import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import styled from '@emotion/styled';
 import { css } from '@emotion/react';
+
+import { getAllTree } from '../../store/reducers/tree-slice';
 
 const Wrapper = styled.div``;
 
 const NaviCp = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllTree());
+  }, [dispatch]);
+
   return (
     <Wrapper>
       <h1>네비</h1>
---
+++ src/store/reducers/tree-slice.js
@@ -0,0 +1,44 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+/** state *************/
+const initialState = {
+  selTree: '',
+  allTree: [],
+};
+
+/** async action ******/
+export const getAllTree = createAsyncThunk('tree/asyncTree', async () => {
+  const url = 'http://127.0.0.1:3100/api/tree';
+  const { data } = await axios.get(url);
+  const tree = data[0].children.map((v) => {
+    let children = v.children.map((v2) => ({ id: v2.id, title: v2.text }));
+    return { id: v.id, title: v.text, children };
+  });
+  return tree;
+});
+
+/** reducer ***********/
+export const treeSlice = createSlice({
+  name: 'tree',
+  initialState,
+  reducers: {
+    setTree: (state, { payload }) => {
+      state.selTree = payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getAllTree.fulfilled, (state, { payload }) => {
+        console.log('fulfilled', payload);
+        state.allTree = payload;
+      })
+      .addCase(getAllTree.rejected, (state, { payload }) => {
+        console.log('rejected', payload);
+      });
+  },
+});
+
+/** method ************/
+export const { setTree } = treeSlice.actions;
+export default treeSlice.reducer;
