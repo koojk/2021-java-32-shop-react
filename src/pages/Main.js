be71353bd301066561fa9f5742d397eb4d453d8e
koojk <kljh0881@gmail.com>
2021-12-02 13:24:53 +0900

    Parallax 개발 중

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,9 +3,11 @@ import styled, { Container } from '../style';
 
 import HeaderCp from '../components/inc/HeaderCp';
 import BannerWrapperCp from '../components/main/BannerWrapperCp';
+import NoticeWrapperCp from '../components/main/NoticeWrapperCp';
 
 const Wrapper = styled(Container)`
   margin: auto;
+  height: 10000px;
 `;
 
 const Main = () => {
@@ -13,6 +15,7 @@ const Main = () => {
     <Wrapper>
       <HeaderCp />
       <BannerWrapperCp />
+      <NoticeWrapperCp />
     </Wrapper>
   );
 };
